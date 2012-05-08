9ed2292303572d3e8f17c6cbc513e20643b6858d
Koen Kooi <koen@dominion.thruhere.net>
2012-05-08 08:56:22 +0200

    feedbrowser: add v2012.05
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>

--- scripts/generate-inc.sh
+++ scripts/generate-inc.sh
@@ -3,6 +3,9 @@
 cat feed-header.txt > config.inc
 sh generate-feedconf.sh >> config.inc
 
+cat 2012.05-header.txt >> config.inc
+sh generate-feedconf.sh >> config.inc
+
 cat core-header.txt >> config.inc
 sh generate-feedconf.sh >> config.inc
 
